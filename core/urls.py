192f5f330b4cf550439126554d1c6432d6883d87
mr-ema <ema.a.v.c2017@gmail.com>
2024-04-26 18:19:10 -0400

    fix: merge problem

--- core/urls.py
+++ core/urls.py
@@ -17,10 +17,8 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from apps.users.views import UserLoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', UserLoginView.as_view(), name='login'),
-    
+    path('', include('apps.users.urls', namespace='users')),
 ]
