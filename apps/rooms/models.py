9772d6f662c08d64036ea64fc4fe3b4735764d6d
mr-ema <ema.a.v.c2017@gmail.com>
2024-04-29 11:48:08 -0400

    actualizar estilos y agregar CRUD para habitaciones

--- apps/rooms/models.py
+++ apps/rooms/models.py
@@ -3,6 +3,9 @@ from django.db import models
 class RoomType(models.Model):
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
+
 
 class Room(models.Model):
     name = models.CharField(max_length=100)
--- apps/rooms/urls.py
+++ apps/rooms/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-
-from apps.rooms.views import RoomCatalogueListView
+from .views import (RoomCatalogueListView, RoomCreateView, RoomUpdateView)
 
 app_name = 'rooms'
 
 urlpatterns = [
     path('', RoomCatalogueListView.as_view(), name='catalogue'),
+    path('edit/<int:room_id>', RoomUpdateView.as_view(), name='edit'),
+    path('create/', RoomCreateView.as_view(), name='create'),
 ]
