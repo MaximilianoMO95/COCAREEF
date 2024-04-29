3f43d465c1e9612ae4d3936c54d3204e9199fc10
mr-ema <ema.a.v.c2017@gmail.com>
2024-04-29 11:14:26 -0400

    cambiar url -> reservations

--- apps/reservations/urls.py
+++ apps/reservations/urls.py
@@ -4,8 +4,8 @@ from .views import (ListReservationsView, PaymentResultView, CheckoutConfirmView
 app_name = 'reservations'
 
 urlpatterns = [
+    path('', ListReservationsView.as_view(), name='list_reservations'),
     path('checkout/<int:room_id>', CheckoutCreateView.as_view(), name='checkout'),
     path('payment/', CheckoutConfirmView.as_view(), name='payment'),
     path('result/', PaymentResultView.as_view(), name='payment_result'),
-    path('list/', ListReservationsView.as_view(), name='list_reservations'),
 ]
