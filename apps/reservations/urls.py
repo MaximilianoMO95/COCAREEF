854988d6ae79b3052b5e8047484c5aa8ae4c298a
mr-ema <ema.a.v.c2017@gmail.com>
2024-05-02 20:48:13 -0400

    agregar htmx y mejorar logica para reservaciones

--- apps/reservations/urls.py
+++ apps/reservations/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', ListReservationsView.as_view(), name='list'),
     path('checkout/<int:room_id>', OrderCreateView.as_view(), name='checkout'),
     path('payment/', OrderConfirmView.as_view(), name='payment'),
-    path('result/', PaymentResultView.as_view(), name='payment_result'),
+    path('payment/status/', PaymentResultView.as_view(), name='payment_status'),
     path('edit/<int:reservation_id>', EditReservationView.as_view(), name='edit'),
     path('delete/<int:reservation_id>', delete_reservation, name='delete'),
 ]
