468c2d1eb6fbb7a5bf44583edbe4b7dfced54b3b
mr-ema <ema.a.v.c2017@gmail.com>
2024-05-05 19:35:57 -0400

    agregar translate rooms app

--- apps/rooms/models.py
+++ apps/rooms/models.py
@@ -1,19 +1,19 @@
-from datetime import date
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 class RoomType(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(_('name'), max_length=50)
 
     def __str__(self):
         return self.name
 
 
 class Room(models.Model):
-    name = models.CharField(max_length=100)
-    capacity = models.IntegerField()
-    description = models.TextField()
-    price = models.PositiveIntegerField()
-    room_type = models.ForeignKey(RoomType, on_delete=models.CASCADE)
+    name = models.CharField(_('name'), max_length=100)
+    capacity = models.IntegerField(_('capacity'))
+    description = models.TextField(_('description'))
+    price = models.PositiveIntegerField(_('price'))
+    room_type = models.ForeignKey(RoomType, on_delete=models.CASCADE, verbose_name=_('room_type'))
 
     def __str__(self):
         return self.name
