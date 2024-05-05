1afe36a0a60ce2af659aaa5679ab36e930b1adf2
mr-ema <ema.a.v.c2017@gmail.com>
2024-05-04 23:35:11 -0400

    mejorar sistema de reservaciones
    Este commit, agrega funcionalidad para crear reservas directamente en el
    panel de admin. Ademas se agrego un boton para confirmar el pago completo
    de la habitacion reservada. Finalmente se agrego un filtro para filtrar
    los reservaciones en base a la fecha.

--- apps/reservations/views.py
+++ apps/reservations/views.py
@@ -2,27 +2,79 @@ from datetime import date
 from urllib.parse import parse_qs
 from django.contrib.auth.decorators import login_required
 from django.core import signing
-from django.http.response import Http404, HttpResponse, JsonResponse
+from django.http.response import (Http404, JsonResponse)
 from django.shortcuts import (redirect, render, get_object_or_404)
+from django.utils import timezone
 from django.views import View
 from django.contrib.admin.options import method_decorator
 from django.contrib.sites.shortcuts import get_current_site
 import django.http as http
 from django.views.generic import ListView
+from django.views.generic.list import Paginator
 
 from apps.rooms.models import Room
 from apps.webpay.views import WebpayAPI
-from .models import Reservation, ReservationPaymentStatus
-from .forms import (OrderCreateForm, ReservationForm)
+from .models import Reservation
+from .forms import (OrderCreateForm, ReservationFilterForm, ReservationForm)
 
 @method_decorator(login_required, name='dispatch')
 class ListReservationsView(ListView):
     model = Reservation
     template_name = 'reservations/admin/list.html'
+    filter_form = ReservationFilterForm
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['filter_form'] = self.filter_form(self.request.GET)
+
+        paginator = Paginator(context['object_list'], 10)
+        page_number = self.request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        context['page_obj'] = page_obj
+
         return context
+
+
+    def get_queryset(self):
+        queryset = super().get_queryset().order_by('id')
+        form = self.filter_form(self.request.GET)
+
+        if form.is_valid():
+            start_date_from = self.request.GET.get('start_date_from')
+            if start_date_from:
+                queryset = queryset.filter(start_date__gte=start_date_from)
+
+            start_date_to = self.request.GET.get('start_date_to')
+            if start_date_to:
+                queryset = queryset.filter(start_date__lte=start_date_to)
+
+        return queryset
+
+
+@method_decorator(login_required, name='dispatch')
+class CreateReservationView(View):
+    template_name = 'reservations/admin/create.html'
+
+    def get(self, request):
+        form = ReservationForm()
+        return render(request, self.template_name, { 'form': form })
+
+
+    def post(self, request):
+        form = ReservationForm(request.POST)
+        if form.is_valid():
+            room: Room = form.cleaned_data['room']
+            start_date: date = form.cleaned_data['start_date']
+            total_days: int = form.cleaned_data['days_of_stay']
+
+            is_room_available = Reservation.objects.is_room_available(room, start_date, total_days);
+            if is_room_available:
+                form.save()
+                return redirect('reservations:list')
+            else:
+                form.add_error('start_date', 'La habitacion no esta disponible para esa fecha.')
+
+        return render(request, self.template_name, { 'form': form })
 
 
 @method_decorator(login_required, name='dispatch')
@@ -50,10 +102,23 @@ class OrderCreateView(View):
     template_name = 'reservations/checkout.html'
 
     def get(self, request, room_id):
-        form = OrderCreateForm(
-            initial={ 'days_of_stay': 1 }
-        )
         room = get_object_or_404(Room, id=room_id)
+        threshold = timezone.now() - timezone.timedelta(hours=24)
+
+        try:
+            reservation = Reservation.objects.get(
+                user=request.user,
+                room=room,
+                created_at__gte=threshold,
+                payment_status__code = 'NP',
+            )
+
+            request.session['reservation_id'] = reservation.pk
+            request.session.set_expiry(24 * 60 * 60)
+            return redirect('reservations:payment')
+        except Reservation.DoesNotExist: pass
+
+        form = OrderCreateForm(initial={ 'days_of_stay': 1 })
         return render(request, self.template_name, { 'form': form, 'room': room })
 
 
@@ -75,6 +140,8 @@ class OrderCreateView(View):
                 )
                 if reservation:
                     request.session['reservation_id'] = reservation.pk
+                    request.session.set_expiry(24 * 60 * 60)
+
                     return redirect('reservations:payment')
             else:
                 form.add_error('start_date', 'La habitacion no esta disponible para esa fecha.')
@@ -82,8 +149,6 @@ class OrderCreateView(View):
         return render(request, self.template_name, { 'form': form, 'room': room })
 
 
-# TODO: Add session id
-# TODO: Check if room is not already booked
 @method_decorator(login_required, name='dispatch')
 class OrderConfirmView(View):
     template_name = 'reservations/confirm-checkout.html'
@@ -110,16 +175,19 @@ class OrderConfirmView(View):
 
 
     def delete(self, request):
+        bad_response = JsonResponse({ 'message': 'failed to cancel reservation' }, status=400)
+        if 'reservation_id' not in request.session:
+            return bad_response
+
         reservation_id = request.session['reservation_id']
         reservation = get_object_or_404(Reservation, id=reservation_id)
+        if request.user != reservation.user:
+            return bad_response
 
-        response = JsonResponse({ 'message': 'failed to cancel reservation' }, status=400)
-        if request.user == reservation.user:
-            reservation.delete()
-            del request.session['reservation_id']
-            response = JsonResponse({ 'message': 'reservation canceled' }, status=200)
-            response['HX-Redirect'] = '/rooms'
-
+        reservation.delete()
+        del request.session['reservation_id']
+        response = JsonResponse({ 'message': 'reservation canceled' }, status=200)
+        response['HX-Redirect'] = '/rooms'
         return response
 
 
@@ -154,17 +222,13 @@ class PaymentResultView(View):
 
 def book_room(request, room_id, start_date, days_of_stay) -> Reservation | None:
     room_instance = Room.objects.get(id=room_id)
-
     if room_instance:
-        payment_status_instance = ReservationPaymentStatus.objects.get(code='NP')
-        if not payment_status_instance: return None
-
         reservation, _ = Reservation.objects.get_or_create(
             user = request.user,
             room = room_instance,
             start_date = start_date,
             days_of_stay = days_of_stay,
-            payment_status = payment_status_instance,
+            payment_status__code = 'NP',
             deposit_percentage = 30
         )
         reservation.save()
@@ -200,3 +264,14 @@ class DetailsReservationView(View):
 
         reservation: Reservation = get_object_or_404(Reservation, id=reservation_id)
         return render(request, self.template_name, { 'reservation': reservation })
+
+
+    def post(self, request, *args, **kwargs):
+        reservation_id = request.POST.get('reservation_id')
+        reservation = get_object_or_404(Reservation, id=reservation_id)
+
+        reservation.update_payment_status('FP')
+
+        return render(request, self.template_name, { 'reservation': reservation })
+
+
