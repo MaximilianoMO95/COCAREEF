3ee0c1bbbfc33b1db05ccb0a4e35acd8e06c0e25
mr-ema <ema.a.v.c2017@gmail.com>
2024-05-05 20:07:22 -0400

    agregar mas tranducciones

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -1,43 +1,57 @@
 from django import forms
-from django.contrib.auth.forms import (AuthenticationForm, UserCreationForm)
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.utils.translation import gettext_lazy as _
 
 from .models import (User, Employee)
-from django import forms
-from django.contrib.auth.forms import UserCreationForm
 
 class UserRegistrationForm(UserCreationForm):
     email = forms.EmailField(required=True)
-    first_name = forms.CharField(max_length=30, required=True, help_text='Required. Ingrese su nombre.')
-    last_name = forms.CharField(max_length=30, required=True, help_text='Required. Ingrese su apellido.')
-    dni = forms.CharField(max_length=15, required=True, help_text='Required. Ingrese su dni.')
+    first_name = forms.CharField(label=_('First Name'), max_length=30, required=True, help_text=_('Required. Please type your name.'))
+    last_name = forms.CharField(label=_('Last Name'), max_length=30, required=True, help_text=_('Required. Please type your last name.'))
+    dni = forms.CharField(max_length=15, required=True, help_text=_('Required. Please type your dni.'))
 
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', 'dni')
+        labels = {
+            'username': _('Username'),
+            'password1': _('Password'),
+            'password2': _('Confirm Password')
+        }
 
 
 class UserLoginForm(AuthenticationForm):
     class Meta:
         model = User
         fields = ('username', 'password')
+        labels = {
+            'username': _('username'),
+            'password': _('password'),
+        }
 
 
 class EmployeeRegistrationForm(UserCreationForm):
     email = forms.EmailField(required=True)
-    first_name = forms.CharField(max_length=30, required=True, help_text='Required. Ingrese su nombre.')
-    last_name = forms.CharField(max_length=30, required=True, help_text='Required. Ingrese su apellido.')
-    rut = forms.CharField(max_length=15, required=True, help_text='Required. Ingrese su RUT.')
-    phone_number = forms.CharField(max_length=9, required=True, help_text='Required. Ingrese su numero de telefono.')
+    first_name = forms.CharField(label=_('First Name'), max_length=30, required=True, help_text=_('Required. Please type your name.'))
+    last_name = forms.CharField(label=_('Last Name'), max_length=30, required=True, help_text=_('Required. Please type your last name.'))
+    rut = forms.CharField(max_length=15, required=True, help_text='Required. Please type your RUT.')
+    phone_number = forms.CharField(label=_('Phone Number'), max_length=9, required=True, help_text='Required. Please type your phone number.')
 
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', 'rut', 'phone_number')
+        labels = {
+            'username': _('Username'),
+            'password1': _('Password'),
+            'password2': _('Confirm Password')
+        }
 
 
 class EmployeeEditForm(forms.ModelForm):
     email = forms.EmailField()
-    phone_number = forms.CharField()
+    phone_number = forms.CharField(label=_('Phone Number'))
 
     class Meta:
         model = Employee
         fields = ['user', 'email', 'phone_number']
+        labels = { 'user': _('User') }
--- apps/users/models.py
+++ apps/users/models.py
@@ -1,17 +1,18 @@
 from django.contrib.auth.models import (AbstractUser)
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
-    first_name = models.CharField(max_length=30, verbose_name='Nombre', blank=False)
-    last_name = models.CharField(max_length=30, verbose_name='Apellido', blank=False)
+    first_name = models.CharField(_('first_name'), max_length=30, blank=False)
+    last_name = models.CharField(_('last_name'), max_length=30, blank=False)
 
 
 class Employee(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    rut  = models.CharField(max_length=15, verbose_name='RUT', blank=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name=_('user'))
+    rut  = models.CharField(max_length=15, blank=False)
 
 
 class Customer(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    dni = models.CharField(max_length=15, verbose_name='DNI', blank=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name=_('user'))
+    dni = models.CharField(max_length=15, blank=False)
